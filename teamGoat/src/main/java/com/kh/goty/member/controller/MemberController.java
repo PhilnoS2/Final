1bddb9ac2526bb8a81e4593c0169b81efe8d8fc8
judeyong <su2x123@gmail.com>
2024-05-13 08:26:29 +0900

    240513

--- teamGoat/src/main/java/com/kh/goty/member/controller/MemberController.java
+++ teamGoat/src/main/java/com/kh/goty/member/controller/MemberController.java
@@ -45,10 +45,18 @@ public class MemberController {
 	@PostMapping("login.member")
 	public ModelAndView login(Member member, HttpSession session, ModelAndView mv) {
 		Member loginMember = memberService.login(member);
+<<<<<<< Updated upstream
 		
 		// 임시코드발급상태확인
 		if(loginMember.getEmptyCodeYn().equals("Y")) {
 			mv.addObject("loginMember", loginMember).setViewName("member/updatePwdForm");
+=======
+		System.out.println(loginMember);
+		
+		// 임시코드발급상태
+		if(loginMember.getEmptyCodeYN() == "Y") {
+			mv.setViewName("redirect:/member/updatePwdForm");
+>>>>>>> Stashed changes
 			return mv;
 		}
 		
