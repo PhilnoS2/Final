fc69a785348032ef795b79b9cba95fd8c7d82bb3
PhilnoS2 <164119126+PhilnoS2@users.noreply.github.com>
2024-05-16 17:42:33 +0900

    qwe

--- teamGoat/src/main/java/com/kh/goty/item/model/service/ItemServiceImpl.java
+++ teamGoat/src/main/java/com/kh/goty/item/model/service/ItemServiceImpl.java
@@ -2,6 +2,24 @@ package com.kh.goty.item.model.service;
 
 import java.util.List;
 
+<<<<<<< HEAD
+import com.kh.goty.common.model.vo.PageInfo;
+import com.kh.goty.item.model.vo.Item;
+
+public interface ItemService {
+
+	int findMachineCount(String platform);
+	
+	int findGameCount(String platform);
+	
+	int findAccessoryCount(String platform);
+	
+	List<Item> findMachineList(String platform, PageInfo pi);
+	
+	List<Item> findGameList(String platform, PageInfo pi);
+	
+	List<Item> findAccessoryList(String platform, PageInfo pi);
+=======
 import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +30,7 @@ import com.kh.goty.item.model.dao.ItemRepository;
 import com.kh.goty.item.model.vo.Item;
 
 @Service
-public class ItemService {
+public class ItemServiceImpl implements ItemService{
 	
 	@Autowired
 	private SqlSession sqlSession;
@@ -21,10 +39,22 @@ public class ItemService {
 	private ItemRepository itemRepository;
 	
 	// List Count용 - 반환  int)
+	@Override
 	public int findMachineCount(String platform) {
 		return itemRepository.findMachineCount(sqlSession, platform);
 	}
 	
+	@Override
+	public int findGameCount(String platform) {
+		return itemRepository.findGameCount(sqlSession, platform);
+	}
+	
+	@Override
+	public int findAccessoryCount(String platform) {
+		return itemRepository.findAccessoryCount(sqlSession, platform);
+	}
+	
+	@Override
 	public List<Item> findMachineList(String platform, PageInfo pi){
 		
 		int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit();
@@ -33,4 +63,15 @@ public class ItemService {
 		return itemRepository.findMachineList(sqlSession, platform , rowBounds);
 	}
 
+	@Override
+	public List<Item> findGameList(String platform, PageInfo pi) {
+		return null;
+	}
+
+	@Override
+	public List<Item> findAccessoryList(String platform, PageInfo pi) {
+		return null;
+	}
+
+>>>>>>> parent of 251d263 (Revert "qwe")
 }
