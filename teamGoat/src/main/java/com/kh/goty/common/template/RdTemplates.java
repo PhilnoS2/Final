4197ee5da7cc6347efd15ed21de22ffdb63a7680
PhilnoS2 <164119126+PhilnoS2@users.noreply.github.com>
2024-05-26 16:21:36 +0900

    cs

---
+++ teamGoat/src/main/java/com/kh/goty/common/template/RdTemplates.java
@@ -0,0 +1,33 @@
+package com.kh.goty.common.template;
+
+import java.nio.charset.Charset;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+import com.kh.goty.board.model.vo.ResponseData;
+
+public class RdTemplates {
+	
+	public static ResponseData getRd(String data, String rCode, String message) {
+		ResponseData rd = null;
+		
+		rd = ResponseData.builder()
+		  		   .data(data)
+		  		   .responseCode(rCode)
+		  		   .message(message)
+		  		   .build();
+		return rd;
+	}
+	
+	
+	public static HttpHeaders getHeader() {
+		
+		  HttpHeaders headers = new HttpHeaders();
+		  headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+		  
+		  return headers;
+	}
+	
+	
+}
